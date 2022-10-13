2f0342b47f636e3d0f3c2ccee5b6f3e8c7a1191f
kolawole adeyemo <kolawole_adeyemo@yahoo.com>
2022-10-13 22:27:30 +0100

    add question

--- js/Quiz.js
+++ js/Quiz.js
@@ -17,4 +17,13 @@ function showQeustion(){
 
 }
 
-function 
+const question = [
+    {
+        question: "What year did Nigeria gain independence?", Answers: [
+            { text: "1960", correct: true}
+            { text: "1906", correct: false}
+            { text: "1958", correct: false}
+            { text: "1963", correct: false}
+        ]
+    }
+]
